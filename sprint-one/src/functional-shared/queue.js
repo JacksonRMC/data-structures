aa4cf97890ff610a362ba012e65c106ac1ee8b64
Hack Reactor Student <student@hackreactor.com>
2017-03-30 10:20:32 -0700

    Functional Shared Complete

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+	count = 0;
+	oldest = 0;
+  _.extend(stack, queueMethods);
+  
+  return stack;
 };
 
-var queueMethods = {};
 
 
+ var queueMethods = {};
+
+	
+	queueMethods.enqueue = function(value){
+		queueMethods[count] = value;
+		count++;
+		
+	}
+
+	queueMethods.dequeue = function() {
+		var temp =	queueMethods[oldest];
+		delete	queueMethods[oldest];
+		oldest++;
+		return temp;
+	}
+
+	queueMethods.size = function () {
+	if(oldest > count){
+      return 0;
+    }
+		return count - oldest;
+	}
+ 
