051983888bd35b19fb6dec9934cd54099083f67f
Hack Reactor Student <student@hackreactor.com>
2017-03-29 19:43:13 -0700

    Solves the functional pattern, que and stack.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,33 @@
 var Queue = function() {
+  console.log('TOP OF FUNCTION');
   var someInstance = {};
-
+  var oldest = 0;
+  var current = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[current] = value;
+    current++;
   };
 
   someInstance.dequeue = function() {
+    var temp = storage[oldest];
+    delete (storage[oldest]); // NEED TO SAVE THIS FIRST
+    oldest++;  
+    
+    return temp;
+
   };
 
   someInstance.size = function() {
+    if(oldest > current){
+      return 0;
+    }
+    return current - oldest;
+
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,25 @@
 var Stack = function() {
   var someInstance = {};
-
+  var count = 0;
+  
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count++;
+    storage[count] = value;
   };
 
   someInstance.pop = function() {
+    if(count > 0){
+      count--;; 
+    }
+    return storage[count + 1];
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
