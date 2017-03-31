2ab1fd7965caf56c959c97826de4d53e60b94e49
Hack Reactor Student <student@hackreactor.com>
2017-03-30 21:16:22 -0700

    Linked Lists

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,22 @@
 var BinarySearchTree = function(value) {
+debugger;
+var searchResults = Object.create(searchMethods);
+storage.count = 0;
+//  var storage = Object.create(stackMethods);
+//  storage.count = 0;
+//  return storage;
+
+return searchResults;
+
 };
+
+var searchMethods = {};
+
+searchMethods.insert = function{};
+
+searchMethods.contains = function{};
+
+searchMethods.depthfirstlog = function{};
 
 
 /*
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+   
+    if(list.head === null){
+      list.head = Node(value);
+    }
+
+    list.tail = Node(value);
+      
   };
 
   list.removeHead = function() {
+    
+    var firstNode = list.head;
+    if (firstNode.next === null) {
+      list.head = list.tail;
+    } else {
+       list.head = firstNode.next;
+    }
+       return firstNode.value;
   };
 
   list.contains = function(target) {
+    var nodeSearch = function(nodeForSearch){
+      if(nodeForSearch.value === target){
+        return true;
+      } else {
+        if(nodeForSearch.next !== null){
+          nodeSearch(nodeForSearch.next);
+        }
+      }
+    }
+    console.log(list.head);
+    nodeSearch(list.head);
+    return false;
   };
 
   return list;
